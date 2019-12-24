1a02fa714820acd52310aa8e3ee6d4ee5facf1a7
Tai Phan <tai101568@gmail.com>
2019-12-24 17:10:24 +0700

    add arrow function

---
+++ lessons/arrow-function.js
@@ -0,0 +1,32 @@
+function func01 ( name , age) {
+  return `My name is ${name } , ${age} years old`
+}
+console.log(func01 ('John',01 ))
+
+// Case 2 : the function assign to variable
+
+var func02 = function( name, age ) {
+  return `My name is ${name } , ${age} years old`
+}
+console.log(func02 ('John',02 ))
+
+// Case 3 : arrow function has 2 params
+var func03 = (name, age) => {
+  return `My name is ${name } , ${age} years old`
+}
+console.log(func03 ('John',03 ))
+
+// Case 4 : only one statement
+
+var func04 = (name, age) => `My name is ${name } , ${age} years old`
+console.log(func04 ('John',04 ))
+
+// Case 5 : only one statement AND one params
+
+var func05 = x => `My name is ${x } , i have 0 year old`
+console.log(func05 ('John'))
+
+// Case 6 : no params
+var func06 = () => `My name is XXX and no XXX year old. because Im hard code`
+console.log(func06 ())
+
